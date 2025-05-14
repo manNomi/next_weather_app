c0cf0847bdb6f4bf0f7e20112ca6282a6b4c1803
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-15 03:31:52 +0900

    feat : pages applo 이용 cleint graphql통한 데이터 가져오기 구현했습니다

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ApolloServer, gql } from "apollo-server-micro";
 import { resolvers, typeDefs } from "./schema";
+import { fetchWeather } from "./lib/fetchWeather";
+
+const cities = ["Seoul", "Tokyo", "Paris"];
+
+let pollingInitialized = false;
+async function initPolling() {
+  if (pollingInitialized) return;
+
+  pollingInitialized = true;
+  for (const city of cities) {
+    await fetchWeather(city);
+  }
+  setInterval(async () => {
+    for (const city of cities) {
+      await fetchWeather(city);
+    }
+  }, 10 * 60 * 1000);
+}
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
@@ -9,6 +27,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await initPolling();
   await startServer;
   await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
 }
--- pages/api/lib/initPolling.ts
+++
@@ -1,7 +0,0 @@
-import { fetchWeather } from "./fetchWeather";
-
-const cities = ["Seoul", "Tokyo", "Paris"];
-cities.forEach(fetchWeather);
-setInterval(() => {
-  cities.forEach(fetchWeather);
-}, 10 * 60 * 1000);

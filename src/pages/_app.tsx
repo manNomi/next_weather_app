3b7a6f9007142cbbee47e018bdd76deb7ab4ef7e
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-16 19:22:26 +0900

    feat : GlobalLayout 반응형 웹을 위한 layout app에 전역으로 추가했습니다

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import GlobalLayout from "@/app/GlobalLayout";
 import "../app/styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <GlobalLayout>
+      <Component {...pageProps} />
+    </GlobalLayout>
+  );
 };
 
 export default App;

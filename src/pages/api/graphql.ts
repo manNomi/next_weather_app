106ed60106efaa2cc6fedcba5dc48109ddd0624e
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-16 15:01:11 +0900

    refactor : app/apollo ApolloClientProvider로 수정했습니다 "

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,33 +1,53 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ApolloServer, gql } from "apollo-server-micro";
-import { resolvers, typeDefs } from "./schema";
-import { fetchWeather } from "./lib/fetchWeather";
+import { GraphQLScalarType, Kind } from "graphql";
 
-const cities = ["Seoul", "Tokyo", "Paris"];
+const API_KEY = process.env.OPENWEATHER_API_KEY;
 
-let pollingInitialized = false;
-async function initPolling() {
-  if (pollingInitialized) return;
+const JSONObject = new GraphQLScalarType({
+  name: "JSON",
+  description: "Arbitrary JSON",
+  parseValue: (value) => value,
+  serialize: (value) => value,
+  parseLiteral: (ast) =>
+    ast.kind === Kind.STRING ? JSON.parse(ast.value) : null,
+});
 
-  pollingInitialized = true;
-  for (const city of cities) {
-    await fetchWeather(city);
+const typeDefs = gql`
+  scalar JSON
+
+  type Query {
+    forecast(city: String!): JSON
   }
-  setInterval(async () => {
-    for (const city of cities) {
-      await fetchWeather(city);
-    }
-  }, 10 * 60 * 1000);
-}
+`;
+const resolvers = {
+  JSON: JSONObject,
+  Query: {
+    forecast: async (_: unknown, { city }: { city: string }) => {
+      const data = await fetchWeather(city);
+      return data;
+    },
+  },
+};
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
+
+export const fetchWeather = async (city: string) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric&lang=kr`
+  );
+  if (response.ok) {
+    await response.json();
+  } else {
+    console.error(`${city} 날씨 정보 갱신 실패`);
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await initPolling();
   await startServer;
   await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
 }
